e8099f67915eaecadb82eeae720d4799d6ecac9c
3333c <0414minyoung@naver.com>
2025-03-16 17:16:43 +0900

    ParticipantDAO 생성

--- src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
+++ src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
@@ -1,4 +1,27 @@
 package org.example.webapp.model.dao;
 
+import org.example.webapp.model.dto.ParticipantDTO;
+
+import java.util.ArrayList;
+
 public class ParticipantDAO {
+    public ArrayList<ParticipantDTO> selectAll(ParticipantDTO participantDTO){
+        return null;
+    }
+
+    public ParticipantDTO selectOne(ParticipantDTO participantDTO){
+        return null;
+    }
+
+    public boolean insert(ParticipantDTO participantDTO){
+        return false;
+    }
+
+    public boolean update(ParticipantDTO participantDTO){
+        return false;
+    }
+
+    public boolean delete(ParticipantDTO participantDTO){
+        return false;
+    }
 }

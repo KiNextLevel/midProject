ee6520939b69b3f3accbc583a519565a00ad1e75
3333c <0414minyoung@naver.com>
2025-03-16 17:16:35 +0900

    BoardDAO 생성

--- src/main/java/org/example/webapp/model/dao/BoardDAO.java
+++ src/main/java/org/example/webapp/model/dao/BoardDAO.java
@@ -1,4 +1,27 @@
 package org.example.webapp.model.dao;
 
+import org.example.webapp.model.dto.BoardDTO;
+
+import java.util.ArrayList;
+
 public class BoardDAO {
+    public ArrayList<BoardDTO> selectAll(BoardDTO boardDTO){
+        return null;
+    }
+
+    public BoardDTO selectOne(BoardDTO boardDTO){
+        return null;
+    }
+
+    public boolean insert(BoardDTO boardDTO){
+        return false;
+    }
+
+    public boolean update(BoardDTO boardDTO){
+        return false;
+    }
+
+    public boolean delete(BoardDTO boardDTO){
+        return false;
+    }
 }

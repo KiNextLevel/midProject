eb13dd09b85a417639fc3032cc951b572dd02529
3333c <0414minyoung@naver.com>
2025-03-16 17:05:02 +0900

    ParticipantDTO 생성

--- src/main/java/org/example/webapp/model/dto/ParticipantDTO.java
+++ src/main/java/org/example/webapp/model/dto/ParticipantDTO.java
@@ -1,4 +1,34 @@
 package org.example.webapp.model.dto;
 
 public class ParticipantDTO {
+    // 참가한 유저의 이벤트 번호
+    private int  PARTICIPANT_BOARD_NUM;
+    // 참가한 유저의 이메일
+    private String PARTICIPANT_USER_EMAIL;
+
+    // getter, setter 메서드
+    public int getPARTICIPANT_BOARD_NUM() {
+        return PARTICIPANT_BOARD_NUM;
+    }
+
+    public void setPARTICIPANT_BOARD_NUM(int PARTICIPANT_BOARD_NUM) {
+        this.PARTICIPANT_BOARD_NUM = PARTICIPANT_BOARD_NUM;
+    }
+
+    public String getPARTICIPANT_USER_EMAIL() {
+        return PARTICIPANT_USER_EMAIL;
+    }
+
+    public void setPARTICIPANT_USER_EMAIL(String PARTICIPANT_USER_EMAIL) {
+        this.PARTICIPANT_USER_EMAIL = PARTICIPANT_USER_EMAIL;
+    }
+
+    // toString 메서드
+    @Override
+    public String toString() {
+        return "ParticipantDTO{" +
+                "PARTICIPANT_BOARD_NUM=" + PARTICIPANT_BOARD_NUM +
+                ", PARTICIPANT_USER_EMAIL='" + PARTICIPANT_USER_EMAIL + '\'' +
+                '}';
+    }
 }

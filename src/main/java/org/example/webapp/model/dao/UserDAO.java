8be9c24cb826b3dfc6e63c50c36260ffd4e235de
3333c <0414minyoung@naver.com>
2025-03-16 17:16:29 +0900

    UserDAO 생성

--- src/main/java/org/example/webapp/model/dao/UserDAO.java
+++ src/main/java/org/example/webapp/model/dao/UserDAO.java
@@ -1,4 +1,27 @@
 package org.example.webapp.model.dao;
 
+import org.example.webapp.model.dto.UserDTO;
+
+import java.util.ArrayList;
+
 public class UserDAO {
+    public ArrayList<UserDTO> selectAll(UserDTO userDTO){
+        return null;
+    }
+
+    public UserDTO selectOne(UserDTO userDTO){
+        return null;
+    }
+
+    public boolean insert(UserDTO userDTO){
+        return false;
+    }
+
+    public boolean update(UserDTO userDTO){
+        return false;
+    }
+
+    public boolean delete(UserDTO userDTO){
+        return false;
+    }
 }

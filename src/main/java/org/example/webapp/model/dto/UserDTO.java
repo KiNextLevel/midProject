46609861127b2863f6a26eefe1c3ed1e225b359f
3333c <0414minyoung@naver.com>
2025-03-17 20:07:33 +0900

    dto 이름 변경

--- src/main/java/org/example/webapp/model/dto/UserDTO.java
+++ src/main/java/org/example/webapp/model/dto/UserDTO.java
@@ -4,216 +4,219 @@ import java.util.Date;
 
 public class UserDTO {
     // 사용자 이메일
-    private String USER_EMAIL;
+    private String userEmail;
     // 사용자 비밀번호
-    private String USER_PASSWORD;
+    private String userPassword;
     // 사용자 닉네임
-    private String USER_NICKNAME;
+    private String userNickname;
     // 회원가입 날짜
-    private Date USER_REGDATE;
+    private Date userRegdate;
     // 성별
-    private boolean USER_GENDER;
+    private boolean userGender;
     // 생년월일
-    private String USER_BIRTH;
+    private String userBirth;
     // 사용자 키
-    private int USER_HEIGHT;
+    private int userHeight;
     // 사용자 체형
-    private String USER_BODY;
+    private String userBody;
     // MBTI
-    private String USER_MBTI;
+    private String userMbti;
     // 회원 사진
-    private String USER_PROFILE;
+    private String userProfile;
     // 학력
-    private String USER_EDUCATION;
+    private String userEducation;
     // 종교
-    private String USER_RELIGION;
+    private String userReligion;
     // 음주
-    private int USER_DRINK;
+    private int userDrink;
     // 흡연
-    private boolean USER_SMOKE;
+    private boolean userSmoke;
     // 직업
-    private String USER_JOB;
+    private String userJob;
     // 역할
-    private String USER_ROLE;
+    private String userRole;
     // 구독 여부
-    private boolean USER_PREMINUM;
+    private boolean userPreminum;
     // 토큰 개수
-    private int USER_TOKEN;
+    private int userToken;
     // 지역
-    private String USER_REGION;
+    private String userRegion;
     // 자기소개
-    private String USER_DESCRIPTION;
+    private String userDescription;
     // 이름
-    private String USER_NAME;
+    private String userName;
     // 컨디션
     private String condition;
+    // 키워드 검색
+    private String searchKeyword;
+
     // Getter, Setter 메서드
-    public String getUSER_EMAIL() {
-        return USER_EMAIL;
+    public String getUserEmail() {
+        return userEmail;
     }
 
-    public void setUSER_EMAIL(String USER_EMAIL) {
-        this.USER_EMAIL = USER_EMAIL;
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
-    public String getUSER_PASSWORD() {
-        return USER_PASSWORD;
+    public String getUserPassword() {
+        return userPassword;
     }
 
-    public void setUSER_PASSWORD(String USER_PASSWORD) {
-        this.USER_PASSWORD = USER_PASSWORD;
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
     }
 
-    public String getUSER_NICKNAME() {
-        return USER_NICKNAME;
+    public String getUserNickname() {
+        return userNickname;
     }
 
-    public void setUSER_NICKNAME(String USER_NICKNAME) {
-        this.USER_NICKNAME = USER_NICKNAME;
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
     }
 
-    public Date getUSER_REGDATE() {
-        return USER_REGDATE;
+    public Date getUserRegdate() {
+        return userRegdate;
     }
 
-    public void setUSER_REGDATE(Date USER_REGDATE) {
-        this.USER_REGDATE = USER_REGDATE;
+    public void setUserRegdate(Date userRegdate) {
+        this.userRegdate = userRegdate;
     }
 
-    public boolean isUSER_GENDER() {
-        return USER_GENDER;
+    public boolean isUserGender() {
+        return userGender;
     }
 
-    public void setUSER_GENDER(boolean USER_GENDER) {
-        this.USER_GENDER = USER_GENDER;
+    public void setUserGender(boolean userGender) {
+        this.userGender = userGender;
     }
 
-    public String getUSER_BIRTH() {
-        return USER_BIRTH;
+    public String getUserBirth() {
+        return userBirth;
     }
 
-    public void setUSER_BIRTH(String USER_BIRTH) {
-        this.USER_BIRTH = USER_BIRTH;
+    public void setUserBirth(String userBirth) {
+        this.userBirth = userBirth;
     }
 
-    public int getUSER_HEIGHT() {
-        return USER_HEIGHT;
+    public int getUserHeight() {
+        return userHeight;
     }
 
-    public void setUSER_HEIGHT(int USER_HEIGHT) {
-        this.USER_HEIGHT = USER_HEIGHT;
+    public void setUserHeight(int userHeight) {
+        this.userHeight = userHeight;
     }
 
-    public String getUSER_BODY() {
-        return USER_BODY;
+    public String getUserBody() {
+        return userBody;
     }
 
-    public void setUSER_BODY(String USER_BODY) {
-        this.USER_BODY = USER_BODY;
+    public void setUserBody(String userBody) {
+        this.userBody = userBody;
     }
 
-    public String getUSER_MBTI() {
-        return USER_MBTI;
+    public String getUserMbti() {
+        return userMbti;
     }
 
-    public void setUSER_MBTI(String USER_MBTI) {
-        this.USER_MBTI = USER_MBTI;
+    public void setUserMbti(String userMbti) {
+        this.userMbti = userMbti;
     }
 
-    public String getUSER_PROFILE() {
-        return USER_PROFILE;
+    public String getUserProfile() {
+        return userProfile;
     }
 
-    public void setUSER_PROFILE(String USER_PROFILE) {
-        this.USER_PROFILE = USER_PROFILE;
+    public void setUserProfile(String userProfile) {
+        this.userProfile = userProfile;
     }
 
-    public String getUSER_EDUCATION() {
-        return USER_EDUCATION;
+    public String getUserEducation() {
+        return userEducation;
     }
 
-    public void setUSER_EDUCATION(String USER_EDUCATION) {
-        this.USER_EDUCATION = USER_EDUCATION;
+    public void setUserEducation(String userEducation) {
+        this.userEducation = userEducation;
     }
 
-    public String getUSER_RELIGION() {
-        return USER_RELIGION;
+    public String getUserReligion() {
+        return userReligion;
     }
 
-    public void setUSER_RELIGION(String USER_RELIGION) {
-        this.USER_RELIGION = USER_RELIGION;
+    public void setUserReligion(String userReligion) {
+        this.userReligion = userReligion;
     }
 
-    public int getUSER_DRINK() {
-        return USER_DRINK;
+    public int getUserDrink() {
+        return userDrink;
     }
 
-    public void setUSER_DRINK(int USER_DRINK) {
-        this.USER_DRINK = USER_DRINK;
+    public void setUserDrink(int userDrink) {
+        this.userDrink = userDrink;
     }
 
-    public boolean isUSER_SMOKE() {
-        return USER_SMOKE;
+    public boolean isUserSmoke() {
+        return userSmoke;
     }
 
-    public void setUSER_SMOKE(boolean USER_SMOKE) {
-        this.USER_SMOKE = USER_SMOKE;
+    public void setUserSmoke(boolean userSmoke) {
+        this.userSmoke = userSmoke;
     }
 
-    public String getUSER_JOB() {
-        return USER_JOB;
+    public String getUserJob() {
+        return userJob;
     }
 
-    public void setUSER_JOB(String USER_JOB) {
-        this.USER_JOB = USER_JOB;
+    public void setUserJob(String userJob) {
+        this.userJob = userJob;
     }
 
-    public String getUSER_ROLE() {
-        return USER_ROLE;
+    public String getUserRole() {
+        return userRole;
     }
 
-    public void setUSER_ROLE(String USER_ROLE) {
-        this.USER_ROLE = USER_ROLE;
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
     }
 
-    public boolean isUSER_PREMINUM() {
-        return USER_PREMINUM;
+    public boolean isUserPreminum() {
+        return userPreminum;
     }
 
-    public void setUSER_PREMINUM(boolean USER_PREMINUM) {
-        this.USER_PREMINUM = USER_PREMINUM;
+    public void setUserPreminum(boolean userPreminum) {
+        this.userPreminum = userPreminum;
     }
 
-    public int getUSER_TOKEN() {
-        return USER_TOKEN;
+    public int getUserToken() {
+        return userToken;
     }
 
-    public void setUSER_TOKEN(int USER_TOKEN) {
-        this.USER_TOKEN = USER_TOKEN;
+    public void setUserToken(int userToken) {
+        this.userToken = userToken;
     }
 
-    public String getUSER_REGION() {
-        return USER_REGION;
+    public String getUserRegion() {
+        return userRegion;
     }
 
-    public void setUSER_REGION(String USER_REGION) {
-        this.USER_REGION = USER_REGION;
+    public void setUserRegion(String userRegion) {
+        this.userRegion = userRegion;
     }
 
-    public String getUSER_DESCRIPTION() {
-        return USER_DESCRIPTION;
+    public String getUserDescription() {
+        return userDescription;
     }
 
-    public void setUSER_DESCRIPTION(String USER_DESCRIPTION) {
-        this.USER_DESCRIPTION = USER_DESCRIPTION;
+    public void setUserDescription(String userDescription) {
+        this.userDescription = userDescription;
     }
 
-    public String getUSER_NAME() {
-        return USER_NAME;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUSER_NAME(String USER_NAME) {
-        this.USER_NAME = USER_NAME;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getCondition() {
@@ -224,31 +227,40 @@ public class UserDTO {
         this.condition = condition;
     }
 
-    // toString 메서드
+    public String getSearchKeyword() {
+        return searchKeyword;
+    }
+
+    public void setSearchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+    }
+
     @Override
     public String toString() {
         return "UserDTO{" +
-                "USER_EMAIL='" + USER_EMAIL + '\'' +
-                ", USER_PASSWORD='" + USER_PASSWORD + '\'' +
-                ", USER_NICKNAME='" + USER_NICKNAME + '\'' +
-                ", USER_REGDATE=" + USER_REGDATE +
-                ", USER_GENDER=" + USER_GENDER +
-                ", USER_BIRTH='" + USER_BIRTH + '\'' +
-                ", USER_HEIGHT=" + USER_HEIGHT +
-                ", USER_BODY='" + USER_BODY + '\'' +
-                ", USER_MBTI='" + USER_MBTI + '\'' +
-                ", USER_PROFILE='" + USER_PROFILE + '\'' +
-                ", USER_EDUCATION='" + USER_EDUCATION + '\'' +
-                ", USER_RELIGION='" + USER_RELIGION + '\'' +
-                ", USER_DRINK=" + USER_DRINK +
-                ", USER_SMOKE=" + USER_SMOKE +
-                ", USER_JOB='" + USER_JOB + '\'' +
-                ", USER_ROLE='" + USER_ROLE + '\'' +
-                ", USER_PREMINUM=" + USER_PREMINUM +
-                ", USER_TOKEN=" + USER_TOKEN +
-                ", USER_REGION='" + USER_REGION + '\'' +
-                ", USER_DESCRIPTION='" + USER_DESCRIPTION + '\'' +
-                ", USER_NAME='" + USER_NAME + '\'' +
+                "userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userNickname='" + userNickname + '\'' +
+                ", userRegdate=" + userRegdate +
+                ", userGender=" + userGender +
+                ", userBirth='" + userBirth + '\'' +
+                ", userHeight=" + userHeight +
+                ", userBody='" + userBody + '\'' +
+                ", userMbti='" + userMbti + '\'' +
+                ", userProfile='" + userProfile + '\'' +
+                ", userEducation='" + userEducation + '\'' +
+                ", userReligion='" + userReligion + '\'' +
+                ", userDrink=" + userDrink +
+                ", userSmoke=" + userSmoke +
+                ", userJob='" + userJob + '\'' +
+                ", userRole='" + userRole + '\'' +
+                ", userPreminum=" + userPreminum +
+                ", userToken=" + userToken +
+                ", userRegion='" + userRegion + '\'' +
+                ", userDescription='" + userDescription + '\'' +
+                ", userName='" + userName + '\'' +
+                ", condition='" + condition + '\'' +
+                ", searchKeyword='" + searchKeyword + '\'' +
                 '}';
     }
 }

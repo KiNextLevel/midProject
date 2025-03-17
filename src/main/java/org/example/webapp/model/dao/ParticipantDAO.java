77dc08ed62f25065bee72d6a053d7860835588da
3333c <0414minyoung@naver.com>
2025-03-17 22:05:31 +0900

    참여자 DAO 기능 생성

--- src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
+++ src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
@@ -1,27 +1,86 @@
 package org.example.webapp.model.dao;
 
+import org.example.webapp.model.common.JDBCUtil;
 import org.example.webapp.model.dto.ParticipantDTO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class ParticipantDAO {
+    final String SELECTALL = "SELECT B.* FROM BOARD B JOIN PARTICIPANT P ON B.BOARD_NUM = P.PARTICIPANT_BOARD_NUM WHERE P.PARTICIPANT_USER_EMAIL = ? ORDER BY B.BOARD_NUM DESC";
+    final String INSERT = "INSERT INTO PARTICIPANT (PARTICIPANT_BOARD_NUM, PARTICIPANT_USER_EMAIL) VALUES (?, ?)";
+    final String DELETE = "DELETE FROM PARTICIPANT WHERE PARTICIPANT_BOARD_NUM = ? AND PARTICIPANT_USER_EMAIL = ?";
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    // 사용자가 참여한 이벤트 목록
     public ArrayList<ParticipantDTO> selectAll(ParticipantDTO participantDTO){
-        return null;
+        ArrayList<ParticipantDTO> list = new ArrayList<>();
+        try {
+            conn = JDBCUtil.connect();
+            pstmt = conn.prepareStatement(SELECTALL);
+            pstmt.setString(1, participantDTO.getParticipantUserEmail());
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                ParticipantDTO dto = new ParticipantDTO();
+                dto.setParticipantBoardNum(rs.getInt("PARTICIPANT_BOARD_NUM"));
+                dto.setParticipantUserEmail(rs.getString("PARTICIPANT_USER_EMAIL"));
+                list.add(dto);
+            }
+            return list;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        } finally {
+            JDBCUtil.disconnect(conn, pstmt);
+        }
     }
 
+    // x
     public ParticipantDTO selectOne(ParticipantDTO participantDTO){
         return null;
     }
 
+    // 참가하기
     public boolean insert(ParticipantDTO participantDTO){
-        return false;
+        ParticipantDAO list = null;
+        try {
+            conn = JDBCUtil.connect();
+            pstmt = conn.prepareStatement(INSERT);
+            pstmt.setInt(1, participantDTO.getParticipantBoardNum());
+            pstmt.setString(2, participantDTO.getParticipantUserEmail());
+            int result = pstmt.executeUpdate();
+            return result > 0;
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        } finally {
+            JDBCUtil.disconnect(conn, pstmt);
+        }
     }
 
+    // x
     public boolean update(ParticipantDTO participantDTO){
         return false;
     }
 
+    // 참가 취소
     public boolean delete(ParticipantDTO participantDTO){
-        return false;
+        try {
+            conn = JDBCUtil.connect();
+            pstmt = conn.prepareStatement(DELETE);
+            pstmt.setInt(1, participantDTO.getParticipantBoardNum());
+            pstmt.setString(2, participantDTO.getParticipantUserEmail());
+            int result = pstmt.executeUpdate();
+            return result > 0;
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        } finally {
+            JDBCUtil.disconnect(conn, pstmt);
+        }
     }
 }

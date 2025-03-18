a5fbec338d576e5a32f93477b06bb7594288ea51
minch13r <0414minyoung@naver.com>
2025-03-18 11:11:20 +0900

    참여자 수 보여지는 기능 생성

--- src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
+++ src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class ParticipantDAO {
     final String SELECTALL = "SELECT B.* FROM BOARD B JOIN PARTICIPANT P ON B.BOARD_NUM = P.PARTICIPANT_BOARD_NUM WHERE P.PARTICIPANT_USER_EMAIL = ? ORDER BY B.BOARD_NUM DESC";
+    final String SELECTONE = "SELECT COUNT(P.PARTICIPANT_USER_EMAIL) FROM PARTICIPANT P JOIN BOARD B ON P.PARTICIPANT_BOARD_NUM = B.BOARD_NUM WHERE B.BOARD_NUM = ?";
     final String INSERT = "INSERT INTO PARTICIPANT (PARTICIPANT_BOARD_NUM, PARTICIPANT_USER_EMAIL) VALUES (?, ?)";
     final String DELETE = "DELETE FROM PARTICIPANT WHERE PARTICIPANT_BOARD_NUM = ? AND PARTICIPANT_USER_EMAIL = ?";
     Connection conn = null;
@@ -39,9 +40,25 @@ public class ParticipantDAO {
         }
     }
 
-    // x
+    // 현재 참여중인 회원 수(COUNT)
     public ParticipantDTO selectOne(ParticipantDTO participantDTO){
-        return null;
+        ParticipantDTO list = null;
+        try {
+            conn = JDBCUtil.connect();
+            pstmt = conn.prepareStatement(SELECTONE);
+            pstmt.setInt(1, participantDTO.getParticipantBoardNumber());
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                list = new ParticipantDTO();
+                list.setParticipantBoardNumber(rs.getInt("PARTICIPANT_BOARD_NUM"));
+            }
+            return list;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        } finally {
+            JDBCUtil.disconnect(conn, pstmt);
+        }
     }
 
     // 참가하기

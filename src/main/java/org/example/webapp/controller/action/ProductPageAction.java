7318c150b117ff2baf7513a6aac4f9f1a4984411
Changheejo <whckdgml78@naver.com>
2025-03-20 12:41:30 +0900

    first commit

---
+++ src/main/java/org/example/webapp/controller/action/ProductPageAction.java
@@ -0,0 +1,16 @@
+package org.example.webapp.controller.action;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.webapp.controller.common.Action;
+import org.example.webapp.controller.common.ActionForward;
+
+public class ProductPageAction implements Action {
+    @Override
+	public ActionForward execute(HttpServletRequest request) {
+		System.out.println("CONT 로그: PRODUCTPAGE ACTION 도착");
+		ActionForward forward = new ActionForward();
+	    forward.setPath("ProductPage.jsp");
+		forward.setRedirect(true);
+		return forward;
+	}
+}
---
+++ src/main/java/org/example/webapp/controller/common/FrontController.java
@@ -0,0 +1,53 @@
+package org.example.webapp.controller.common;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ActionFactory factory;
+
+    public FrontController() {
+        super();
+        factory = new ActionFactory();
+    }
+
+    private void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String command = request.getRequestURI();
+    	System.out.println(" CTRL 로그 : command ["+command+"]");
+    	command = command.replace("/mid/theme/", "");
+    	System.out.println(" CTRL 로그 : command ["+command+"]");
+    	
+    	Action action = factory.getAction(command); // 팩토리 패턴
+    	ActionForward forward = action.execute(request);
+    	
+    	if(forward == null){
+    		// 에러페이지로 이동
+    	}
+    	
+    	if(forward.isRedirect()){
+    		response.sendRedirect(forward.getPath());
+    	}
+    	else{
+    		RequestDispatcher dispatcher= request.getRequestDispatcher(forward.getPath());
+    		dispatcher.forward(request, response);
+    	}
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("로그 : GET 요청 호출됨");
+		doAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("로그 : POST 요청 호출됨");
+		doAction(request, response);
+	}
+}

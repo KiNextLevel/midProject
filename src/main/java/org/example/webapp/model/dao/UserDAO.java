9d57cac5f6c596e6fefd86e81494a9e4649be85a
minch13r <0414minyoung@naver.com>
2025-03-21 14:09:30 +0900

    fix

--- src/main/java/org/example/webapp/model/dao/UserDAO.java
+++ src/main/java/org/example/webapp/model/dao/UserDAO.java
@@ -158,6 +158,10 @@ public class UserDAO {
                     data.setUserEmail(rs.getString("USER_EMAIL"));
                     if (userDTO.getCondition().equals("SELECTONE")) {
                         data.setUserPassword(rs.getString("USER_PASSWORD"));
+                        data.setUserRole(rs.getInt("USER_ROLE"));
+                    } else if (userDTO.getCondition().equals("SELECTONE_USERINFO")) {
+                        data.setUserPassword(rs.getString("USER_PASSWORD"));
+                        data.setUserName(rs.getString("USER_NAME"));
                         data.setUserNickname(rs.getString("USER_NICKNAME"));
                         data.setUserPhone(rs.getString("USER_PHONE"));
                         data.setUserRegdate(rs.getDate("USER_REGDATE"));
@@ -177,7 +181,6 @@ public class UserDAO {
                         data.setUserToken(rs.getInt("USER_TOKEN"));
                         data.setUserRegion(rs.getString("USER_REGION"));
                         data.setUserDescription(rs.getString("USER_DESCRIPTION"));
-                        data.setUserName(rs.getString("USER_NAME"));
                     }
                 }
             }

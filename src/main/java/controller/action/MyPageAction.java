107a69677e6ed723094dcb4e077f4241c60a906d
minch13r <0414minyoung@naver.com>
2025-03-25 10:10:22 +0900

    마이페이지 수정

--- src/main/java/controller/action/MyPageAction.java
+++ src/main/java/controller/action/MyPageAction.java
@@ -4,46 +4,54 @@ import controller.common.Action;
 import controller.common.ActionForward;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.example.webapp.model.dao.PreferenceDAO;
 import org.example.webapp.model.dao.UserDAO;
+import org.example.webapp.model.dto.PreferenceDTO;
 import org.example.webapp.model.dto.UserDTO;
 
 import java.util.ArrayList;
 
-public class MyPageAction implements Action{
+public class MyPageAction implements Action {
 
-	@Override
-	public ActionForward execute(HttpServletRequest request) {
-		System.out.println("CTRL 로그: MyPageAction");
-		ActionForward forward = new ActionForward();
-		HttpSession session = request.getSession();
-		String userEmail = (String)session.getAttribute("userEmail");
+    @Override
+    public ActionForward execute(HttpServletRequest request) {
+        System.out.println("CTRL 로그: MyPageAction");
+        ActionForward forward = new ActionForward();
+        HttpSession session = request.getSession();
+        String userEmail = (String) session.getAttribute("userEmail");
 
-		if(userEmail == null) {
-			// 로그인되지 않은 경우
-			System.out.println("Mypage Action Log: userEmail is null");
-			forward.setPath("loginPage.do");
-			forward.setRedirect(true);
-			return forward;
-		}
+        if (userEmail == null) {
+            // 로그인되지 않은 경우
+            System.out.println("Mypage Action Log: userEmail is null");
+            forward.setPath("loginPage.do");
+            forward.setRedirect(true);
+            return forward;
+        }
 
-		UserDTO userDTO = new UserDTO();
-		UserDAO userDAO = new UserDAO();
-		userDTO.setUserEmail(userEmail);
-		userDTO.setCondition("SELECTONE_USERINFO"); // 조건 설정 추가
+        UserDTO userDTO = new UserDTO();
+        UserDAO userDAO = new UserDAO();
+        userDTO.setUserEmail(userEmail);
+        userDTO.setCondition("SELECTONE_USERINFO"); // 조건 설정 추가
 
-		UserDTO user = userDAO.selectOne(userDTO);
+        PreferenceDTO preferenceDTO = new PreferenceDTO();
+        PreferenceDAO preferenceDAO = new PreferenceDAO();
+        preferenceDTO.setUserEmail(userEmail);
+        preferenceDTO.setCondition("SELECTONE");
 
-		if(user != null) {
-			request.setAttribute("data", user); // 첫 번째 사용자 정보
-			forward.setPath("MyPage.jsp");
-			forward.setRedirect(false);
-		}
-		else {
-			System.out.println("마이페이지 로그[사용자 정보 없음]");
-			// 사용자 정보가 없을 때 처리
-			forward.setPath("loginPage.do");
-			forward.setRedirect(true);
-		}
-		return forward;
-	}
-	}
+        UserDTO user = userDAO.selectOne(userDTO);
+        PreferenceDTO preference = preferenceDAO.selectOne(preferenceDTO);
+
+        if (user != null) {
+            request.setAttribute("userDTO", user); // 첫 번째 사용자 정보
+            request.setAttribute("preferenceDTO", preference);
+            forward.setPath("/Metronic-Shop-UI-master/theme/MyPage.jsp");
+            forward.setRedirect(false);
+        } else {
+            System.out.println("마이페이지 로그[사용자 정보 없음]");
+            // 사용자 정보가 없을 때 처리
+            forward.setPath("loginPage.do");
+            forward.setRedirect(true);
+        }
+        return forward;
+    }
+}

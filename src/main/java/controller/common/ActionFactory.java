30c9a16c942a63f59b229cbbd58714804f4630f4
minch13r <0414minyoung@naver.com>
2025-03-27 13:42:36 +0900

    봉균 민영 pr

--- src/main/java/controller/common/ActionFactory.java
+++ src/main/java/controller/common/ActionFactory.java
@@ -41,7 +41,6 @@ public class ActionFactory {
 		factory.put("naverCallback.do", new NaverCallBackAction());
 		factory.put("adminPage.do", new AdminMainPageAction());
 		factory.put("tossPaymentPage.do", new TossPaymentPageAction());
-		//factory.put("sendMessage.do", new SendMessageAction());
 		factory.put("updateProfile.do", new UpdateProfileAction());
 		factory.put("updateProfilePage.do", new UpdateProfilePageAction());
 		factory.put("updateProfileImage.do", new UpdateProfileImageAction());

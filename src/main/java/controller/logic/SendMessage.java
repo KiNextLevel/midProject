9617bf2a42290da5e554636a505c21fff5c8372a
유봉균 <bong990812@naver.com>
2025-03-28 16:21:29 +0900

    결제하면 결제한 사람 이메일로 결제 정보 전송

--- src/main/java/controller/logic/SendMessage.java
+++ src/main/java/controller/logic/SendMessage.java
@@ -9,8 +9,8 @@ public class SendMessage {
     final DefaultMessageService messageService;
     public SendMessage() {
         // 반드시 계정 내 등록된 유효한 API 키, API Secret Key를 입력해주셔야 합니다!
-        this.messageService = NurigoApp.INSTANCE.initialize("NCS",
-                "YUQNR",
+        this.messageService = NurigoApp.INSTANCE.initialize("NCSLFOCVDKQN2IJW",
+                "YOUR7XFOD6LZ69EC9PWZ6UWFPL9EUQNR",
                 "https://api.coolsms.co.kr");
     }
     public void sendOne() {
@@ -18,7 +18,7 @@ public class SendMessage {
         // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
         message.setFrom("01095534998");
         message.setTo("01095534998");
-        message.setText("테스트문자");
+        message.setText("관리자 로그인 성공");
         SingleMessageSentResponse response =
                 this.messageService.sendOne(new SingleMessageSendingRequest(message));
         System.out.println(response);

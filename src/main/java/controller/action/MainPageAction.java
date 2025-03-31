ad318b6ca792f4b7368798530742f54778aff327
Ben <mque220@gmail.com>
2025-03-31 17:31:20 +0900

    mainpage filter, pagenation added

--- src/main/java/controller/action/MainPageAction.java
+++ src/main/java/controller/action/MainPageAction.java
@@ -1,5 +1,7 @@
 package controller.action;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import controller.common.Action;
 import controller.common.ActionForward;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,62 +10,89 @@ import org.example.webapp.model.dao.AlertDAO;
 import org.example.webapp.model.dao.UserDAO;
 import org.example.webapp.model.dto.AlertDTO;
 import org.example.webapp.model.dto.UserDTO;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 
 public class MainPageAction implements Action {
-    @Override
-    public ActionForward execute(HttpServletRequest request) {
+	@Override
+	public ActionForward execute(HttpServletRequest request) {
 		System.out.println("CONT 로그: MAINPAGE ACTION 도착");
 		ActionForward forward = new ActionForward();
 		HttpSession session = request.getSession();
 		String userEmail = (String) session.getAttribute("userEmail");
-        //넘어가야하는 정보: 비동기로 처리할듯
-        //1. 알람
-	    //2. 메시지(최프)
-	    AlertDAO alertDAO = new AlertDAO();
-	    AlertDTO alertDTO = new AlertDTO();
-	    alertDTO.setUserEmail(userEmail);
-	    ArrayList<AlertDTO> alertDatas = alertDAO.selectAll(alertDTO);
+		//넘어가야하는 정보: 비동기로 처리할듯
+		//1. 알람
+		//2. 메시지(최프)
+		AlertDAO alertDAO = new AlertDAO();
+		AlertDTO alertDTO = new AlertDTO();
+		alertDTO.setUserEmail(userEmail);
+		ArrayList<AlertDTO> alertDatas = alertDAO.selectAll(alertDTO);
 		for(AlertDTO alertDTO1 : alertDatas) {
 			System.out.println(alertDTO1);
 		}
 		session.setAttribute("alertDatas", alertDatas);
-	    if (alertDatas != null) {
-	    	//알람이 있음
-	    	for(AlertDTO data : alertDatas) {
-	    		if (!data.isAlertIsWatch()) { // 열람한적 없는 데이터가 있다면
-	    			request.setAttribute("hasUnreadAlerts", true);
+		if (alertDatas != null) {
+			//알람이 있음
+			for(AlertDTO data : alertDatas) {
+				if (!data.isAlertIsWatch()) { // 열람한적 없는 데이터가 있다면
+					request.setAttribute("hasUnreadAlerts", true);
 					break;
-	    		}
-	    	}
-	    }
+				}
+			}
+		}
 		UserDAO userDAO = new UserDAO();
 		UserDTO userDTO = new UserDTO();
 		userDTO.setUserEmail(userEmail);
 		userDTO.setCondition("SELECTALL");
 		ArrayList<UserDTO> userDatas = userDAO.selectAll(userDTO);
-		session.setAttribute("userDatas", userDatas);
 
-		// 현재 로그인한 사용자의 정보를 가져옴
-		UserDTO currentUserDTO = new UserDTO();
-		currentUserDTO.setUserEmail(userEmail);
-		currentUserDTO.setCondition("SELECTONE_USERINFO");
-		UserDTO userData = userDAO.selectOne(currentUserDTO);
+		JSONArray userDatasJsonArray = new JSONArray();
 
-		// 세션에 현재 사용자 정보 저장
-		session.setAttribute("userDTO", userData);
+		// userDatas 리스트를 순회하며 JSON 객체로 변환합니다.
+		for (UserDTO user : userDatas) {
+			JSONObject userJson = new JSONObject();
 
-		// 디버깅용 로그 출력
-		if (userData != null) {
-			System.out.println("사용자 이메일: " + userData.getUserEmail());
-			System.out.println("프리미엄 상태: " + userData.isUserPreminum());
-		} else {
-			System.out.println("사용자 정보를 가져오지 못했습니다.");
+			// UserDTO의 각 필드를 JSON 객체에 추가합니다.
+			userJson.put("socialType", user.getSocialType());
+			userJson.put("userPhone", user.getUserPhone());
+			userJson.put("userEmail", user.getUserEmail());
+			userJson.put("userPassword", user.getUserPassword());
+			userJson.put("userNickname", user.getUserNickname());
+			userJson.put("userRegdate", user.getUserRegdate());
+			userJson.put("userGender", user.getUserGender());
+			userJson.put("userBirth", user.getUserBirth());
+			userJson.put("userHeight", user.getUserHeight());
+			userJson.put("userBody", user.getUserBody());
+			userJson.put("userMbti", user.getUserMbti());
+			userJson.put("userProfile", user.getUserProfile());
+			userJson.put("userEducation", user.getUserEducation());
+			userJson.put("userReligion", user.getUserReligion());
+			userJson.put("userDrink", user.getUserDrink());
+			userJson.put("userSmoke", user.isUserSmoke());
+			userJson.put("userJob", user.getUserJob());
+			userJson.put("userRole", user.getUserRole());
+			userJson.put("userPreminum", user.isUserPreminum());
+			userJson.put("userToken", user.getUserToken());
+			userJson.put("userRegion", user.getUserRegion());
+			userJson.put("userDescription", user.getUserDescription());
+			userJson.put("userName", user.getUserName());
+
+			// JSON 배열에 추가합니다.
+			userDatasJsonArray.add(userJson);
 		}
+
+		// JSON 배열을 문자열로 변환해서 리턴합니다.
+		userDatasJsonArray.toJSONString();
+
+		// 전체 사용자 데이터 배열을 JSON 문자열로 변환
+		String jsonUserDatas = userDatasJsonArray.toJSONString();
+		request.setAttribute("userDatas", jsonUserDatas);
+//        request.setAttribute("userDatas", jsonUserDatas);
 
 		forward.setPath("/Metronic-Shop-UI-master/theme/MainPage.jsp");
 		forward.setRedirect(false);
 		return forward;
-    }
+	}
 }
--- src/main/java/controller/asyn/JspFilter.java
+++ src/main/java/controller/asyn/JspFilter.java
@@ -1,80 +1,63 @@
-//package controller.asyn;
-//
-//import jakarta.servlet.*;
-//import jakarta.servlet.annotation.WebFilter;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//
-//import java.io.IOException;
-//import java.io.UnsupportedEncodingException;
-//import java.net.URLEncoder;
-//
-//@WebFilter("*.jsp")  // 모든 .jsp 파일에 대해 필터 적용
-//public class JspFilter implements Filter {
-//
-//    public JspFilter() {
-//        super();
-//    }
-//
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//            throws IOException, ServletException {
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        HttpServletResponse httpResponse = (HttpServletResponse) response;
-//        String requestURI = httpRequest.getRequestURI();
-//        String pageName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
-//
-//        // 세션에서 로그인된 사용자 정보 확인
-//        String userEmail = (String) httpRequest.getSession().getAttribute("userEmail");
-//        Integer userRole = (Integer) httpRequest.getSession().getAttribute("userRole");
-//        System.out.println("pageName: " + pageName);
-//        System.out.println("userRole: " + userRole);
-//
-//        if (pageName.equals("alert.jsp")) {
-//        } else if (userEmail == null && !pageName.equals("Index.jsp") && !pageName.equals("Login.jsp")) {
-//            System.out.println("User not logged in");// 한글 문자 인코딩
-//            String msg = "로그인 후 이용해주세요";
-//            String url = "login.do";
-//
-////            try {
-//                msg = URLEncoder.encode(msg, "UTF-8");
-//                url = URLEncoder.encode(url, "UTF-8");
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-////            httpResponse.sendRedirect("/Metronic-Shop-UI-master/theme/alert.jsp?msg=" + msg + "&url=" + url);
-////            httpResponse.sendRedirect("alert.jsp?msg=" + msg + "&url=" + url);
-////            httpResponse.sendRedirect("alert.jsp");
-////            httpResponse.sendRedirect("loginPage.do");
-//            return;
-//        }
-//        if (userEmail != null){
-//            if (pageName.equals("Index.jsp") || pageName.equals("Login.jsp")) {
-////                System.out.println("Request:" + request);
-//                System.out.println("response:" + response);
-//                String msg = "이미 로그인 한 상태입니다";
-//                String url = "mainPage.do";
-//                try {
-//                    msg = URLEncoder.encode(msg, "UTF-8");
-//                    url = URLEncoder.encode(url, "UTF-8");
-//                } catch (UnsupportedEncodingException e) {
-//                    e.printStackTrace();
-//                }
-//                httpResponse.sendRedirect("/Metronic-Shop-UI-master/theme/alert.jsp?msg=" + msg + "&url=" + url);
-//                return;
-//            } else if (userRole == 0 && pageName.contains("Admin")) {
-//                request.setAttribute("msg", "관리자 전용 페이지 입니다");
-//                request.setAttribute("url", "mainPage.do");
-////                httpResponse.sendRedirect("/Metronic-Shop-UI-master/theme/alert.jsp");
-//                request.getRequestDispatcher("/Metronic-Shop-UI-master/theme/alert.jsp").forward(request, response);
-//                return;
-//            }
-//        }
-//
-//        // 로그인된 사용자의 경우 요청을 계속 진행
-//        chain.doFilter(request, response);
-//    }
-//
-//    public void destroy() {
-//        // 필터 종료 시 클린업 작업
-//    }
-//}
+package controller.asyn;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@WebFilter("*.jsp")  // 모든 .jsp 파일에 대해 필터 적용
+public class JspFilter implements Filter {
+
+    public JspFilter() {
+        super();
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String requestURI = httpRequest.getRequestURI();
+        String pageName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
+
+        // 세션에서 로그인된 사용자 정보 확인
+        String userEmail = (String) httpRequest.getSession().getAttribute("userEmail");
+        Integer userRole = (Integer) httpRequest.getSession().getAttribute("userRole");
+        System.out.println("pageName: " + pageName);
+        System.out.println("userRole: " + userRole);
+
+//        if (pageName.equals("Index.jsp")) {
+//        } else
+        if (userEmail == null && !pageName.equals("Index.jsp") && !pageName.equals("Login.jsp")) {
+            System.out.println("User not logged in");
+            String msg = "로그인 후 이용해주세요";
+            String url = "Index.jsp";
+            httpResponse.sendRedirect(url);
+            return;
+        }
+        if (userEmail != null) {
+            if (pageName.equals("Index.jsp") || pageName.equals("Login.jsp")) {
+                System.out.println("response:" + response);
+                String msg = "이미 로그인 한 상태입니다";
+                String url = "mainPage.do";
+                httpResponse.sendRedirect(url);
+                return;
+            } else if (userRole == 0 && pageName.contains("Admin")) {
+                request.setAttribute("msg", "관리자 전용 페이지 입니다");
+                String url = "mainPage.do";
+                httpResponse.sendRedirect(url);
+                return;
+            }
+        }
+
+        // 로그인된 사용자의 경우 요청을 계속 진행
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        // 필터 종료 시 클린업 작업
+    }
+}

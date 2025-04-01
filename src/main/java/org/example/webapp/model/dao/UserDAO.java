4f78afd61e3197148ef9d643538db054e9f0a82c
Ben <mque220@gmail.com>
2025-04-01 13:57:53 +0900

    login logic fixed

--- src/main/java/org/example/webapp/model/dao/UserDAO.java
+++ src/main/java/org/example/webapp/model/dao/UserDAO.java
@@ -10,9 +10,9 @@ import java.util.ArrayList;
 
 public class UserDAO {
     // 아이디 중복 검사
-    final String SELECTONE_CHECK = "SELECT USER_EMAIL FROM USER WHERE USER_EMAIL = ?";
+    final String SELECTONE_CHECK = "SELECT USER_EMAIL FROM USER WHERE USER_EMAIL = ? AND SOCIAL_TYPE = ?";
     // 로그인
-    final String SELECTONE = "SELECT USER_EMAIL, USER_PASSWORD, USER_ROLE, USER_PREMIUM FROM USER WHERE USER_EMAIL = ? AND USER_PASSWORD = ?";
+    final String SELECTONE = "SELECT USER_EMAIL, USER_PASSWORD, USER_ROLE, USER_PREMIUM FROM USER WHERE USER_EMAIL = ? AND USER_PASSWORD = ? AND SOCIAL_TYPE = ?";
     // 유저 전체 정보 불러오기
     final String SELECTONE_USERINFO = "SELECT * FROM USER WHERE USER_EMAIL = ?";
 
@@ -155,6 +155,7 @@ public class UserDAO {
                 if (userDTO.getCondition().equals("SELECTONE_CHECK")) {
                     pstmt = conn.prepareStatement(SELECTONE_CHECK);
                     pstmt.setString(1, userDTO.getUserEmail());
+                    pstmt.setString(2, userDTO.getSocialType());
                     // 쿼리 실행 및 결과 처리
                     rs = pstmt.executeQuery();
                     if (rs.next()) {
@@ -171,6 +172,7 @@ public class UserDAO {
                     pstmt = conn.prepareStatement(SELECTONE);
                     pstmt.setString(1, userDTO.getUserEmail());
                     pstmt.setString(2, userDTO.getUserPassword());
+                    pstmt.setString(3, userDTO.getSocialType());
 
                     // 쿼리 실행
                     rs = pstmt.executeQuery();

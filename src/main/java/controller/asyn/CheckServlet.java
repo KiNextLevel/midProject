f32972f0d3dea9ae41b440941833d93c4d4eed00
minch13r <0414minyoung@naver.com>
2025-04-02 09:09:16 +0900

    이메일 비동기

---
+++ src/main/java/controller/asyn/CheckServlet.java
@@ -0,0 +1,9 @@
+package controller.asyn;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/checkEmail.do")
+public class CheckServlet extends HttpServlet {
+
+}

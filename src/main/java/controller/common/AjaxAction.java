6e9d7b26ec74487f35016a0630e6bb4c807ffe54
minch13r <0414minyoung@naver.com>
2025-04-03 09:09:31 +0900

    단위 테스트

--- src/main/java/controller/common/AjaxAction.java
+++ src/main/java/controller/common/AjaxAction.java
@@ -8,5 +8,6 @@ import java.io.IOException;
 
 // 비동기용 Action
 public interface AjaxAction {
+    // 기존 Action은 request만 있었는데 응답까지 하는 AjaxAction 생성
     void executeAjax(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }

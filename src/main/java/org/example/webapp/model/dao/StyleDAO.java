8539be9594e60a412a857d11c9f7ec43845d9f9a
Ben <mque220@gmail.com>
2025-04-04 09:22:00 +0900

    DAO - 사용안하는 메서드 private으로 바꿈 mainPage - 종교 추가

--- src/main/java/org/example/webapp/model/dao/StyleDAO.java
+++ src/main/java/org/example/webapp/model/dao/StyleDAO.java
@@ -3,32 +3,32 @@ package org.example.webapp.model.dao;
 import org.example.webapp.model.dto.StyleDTO;
 
 public class StyleDAO {
-    final String SELECTONE = "";
-    final String SELECTALL = "";
-    final String INSERT = "";
-    final String UPDATE="";
-    final String DELETE="";
+    private final String SELECTONE = "";
+    private final String SELECTALL = "";
+    private final String INSERT = "";
+    private final String UPDATE="";
+    private final String DELETE="";
 
-    public StyleDTO selectOne(StyleDTO styleDTO) {
+    private StyleDTO selectOne(StyleDTO styleDTO) {
         return null;
 
     }
 
-    public StyleDTO selectAll(StyleDTO styleDTO) {
+    private StyleDTO selectAll(StyleDTO styleDTO) {
         return null;
 
     }
 
-    public boolean insert(StyleDTO styleDTO) {
+    private boolean insert(StyleDTO styleDTO) {
         return false;
 
     }
 
-    public boolean update(StyleDTO styleDTO) {
+    private boolean update(StyleDTO styleDTO) {
         return false;
     }
 
-    public boolean delete(StyleDTO styleDTO) {
+    private boolean delete(StyleDTO styleDTO) {
         return false;
     }
 }

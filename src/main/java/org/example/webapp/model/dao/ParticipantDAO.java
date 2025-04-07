72a143d49d27f30d7cfd66a7a4fca1559b65daa3
minch13r <0414minyoung@naver.com>
2025-04-07 12:41:37 +0900

    주석

--- src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
+++ src/main/java/org/example/webapp/model/dao/ParticipantDAO.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 public class ParticipantDAO {
     // 사용자가 참여한 이벤트 목록
     private final String SELECTALL = "SELECT B.* , P.PARTICIPANT_USER_EMAIL FROM BOARD B JOIN PARTICIPANT P ON B.BOARD_NUM = P.PARTICIPANT_BOARD_NUM WHERE P.PARTICIPANT_USER_EMAIL = ? ORDER BY B.BOARD_NUM DESC";
+
+    // 사용자가 참여한 이벤트 목록
     private final String SELECTALL_EVENTPRINT =
             "SELECT B.BOARD_TITLE " +
                     "FROM PARTICIPANT P " +
